9e07eaeac381e6a9217be8beee20b865d8247ba5
Primaleads <info@primaleads.de>
2026-03-11 18:22:05 +0100

    Refactor index.js by removing unused code
    Removed Supabase client initialization and geolocation handling code.

--- index.js
+++ index.js
@@ -1,43 +1,3 @@
-const supabaseUrl = "https://vnfnclfqgcqlofjzefye.supabase.co"
-const supabaseKey = "sb_publishable_q3gMEue0WevkMEEwGzGv-w_jE9eFdNr"
-
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
-
-let userLat
-let userLng
-let map
-
-function startRoute(){
-
-map = L.map('map').setView([48.62,9.05],16)
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:19
-}).addTo(map)
-
-navigator.geolocation.getCurrentPosition(position => {
-
-userLat = position.coords.latitude
-userLng = position.coords.longitude
-
-map.setView([userLat,userLng],18)
-
-L.marker([userLat,userLng])
-.addTo(map)
-.bindPopup("Du bist hier")
-.openPopup()
-
-})
-
-}
-
-async function saveLead(status){
-
-const street = document.getElementById("street").value
-const number = document.getElementById("number").value
-
-L.marker([userLat,userLng]).addTo(map)
-
 await supabaseClient
 .from("Leads")
 .insert([
@@ -49,7 +9,3 @@ lat: userLat,
 lng: userLng
 }
 ])
-
-alert("Lead gespeichert")
-
-}
