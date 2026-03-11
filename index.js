461deba83ca0d15528b9f0f8e449e4ea6428bab0
Primaleads <info@primaleads.de>
2026-03-11 18:04:31 +0100

    Implement geolocation and lead saving feature
    Added geolocation functionality to display user location on the map and save lead information to Supabase.

--- index.js
+++ index.js
@@ -2,3 +2,46 @@ const supabaseUrl = "https://vnfnclfqgcqlofjzefye.supabase.co"
 const supabaseKey = "sb_publishable_q3gMEue0WevkMEEwGzGv-w_jE9eFdNr"
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+let userLat
+let userLng
+
+const map = L.map('wurzel').setView([48.62, 9.05], 16)
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19
+}).addTo(map)
+
+navigator.geolocation.getCurrentPosition(position => {
+
+userLat = position.coords.latitude
+userLng = position.coords.longitude
+
+map.setView([userLat, userLng], 18)
+
+L.marker([userLat, userLng])
+.addTo(map)
+.bindPopup("Du bist hier")
+.openPopup()
+
+})
+
+async function saveLead(status){
+
+const street = document.querySelector("input[placeholder='Straße']").value
+const number = document.querySelector("input[placeholder='Hausnummer']").value
+
+const marker = L.marker([userLat, userLng]).addTo(map)
+
+await supabaseClient
+.from("Leads")
+.insert([
+{
+Straße: street,
+Nummer: number,
+Status: status,
+lat: userLat,
+lng: userLng
+}
+])
+
+}
