3d08d7e40e8fcaa8c0e6adc9d8fd86f85769c304
Primaleads <info@primaleads.de>
2026-03-15 18:10:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
 self.addEventListener("install", function(event) {
-self.skipWaiting()
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(self.clients.claim())
 })
 
 self.addEventListener("fetch", function(event) {
-event.respondWith(fetch(event.request))
+  event.respondWith(fetch(event.request))
 })
